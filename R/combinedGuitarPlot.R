dd0b3bcb17e90c88945bbd1ce1a351653a223636
Jia Meng <jia.meng@hotmail.com>
2016-07-17 10:05:22 +0000

    v1.11.9
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Guitar@119412 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/combinedGuitarPlot.R
+++ R/combinedGuitarPlot.R
@@ -1,4 +1,4 @@
-combinedGuitarPlot <- function(ct){ 
+combinedGuitarPlot <- function(ct, comLength = c(0.136,0.459, 0.405)){ 
   # stack
   adjust=1
   
@@ -38,24 +38,45 @@ combinedGuitarPlot <- function(ct){
     annotate("rect", xmin = 0, xmax = 1, ymin = -0.12, ymax = -0.08, alpha = .99, colour = "black")+
     theme(legend.position="bottom")
   
-  pos_adjust <- match(ct1$comp,c("UTR5","CDS","UTR3"))-1
-  ct1$pos <- ct1$pos + pos_adjust
-  p1 <- 
-    ggplot(ct1, aes(x=pos, group=Feature,colour=factor(Feature), weight=3*weight))  +
-    ggtitle("Distribution on mRNA") +
-    theme(axis.ticks = element_blank(), axis.text.x = element_blank()) + 
-    xlab("") + 
-    ylab("Frequency") +
-    geom_density(adjust=adjust,aes(fill=factor(Feature)),alpha=0.2) +
-    annotate("text", x = 0.5, y = -0.2, label = "5'UTR") +
-    annotate("text", x = 1.5, y = -0.2, label = "CDS") +
-    annotate("text", x = 2.5, y = -0.2, label = "3'UTR") + 
-    geom_vline(xintercept=1:2, linetype="dotted") + 
-    theme(legend.position="bottom") +
-    annotate("rect", xmin = 0, xmax = 1, ymin = -0.12, ymax = -0.08, alpha = .99, colour = "black")+
-    annotate("rect", xmin = 2, xmax = 3, ymin = -0.12, ymax = -0.08, alpha = .99, colour = "black")+
-    annotate("rect", xmin = 1, xmax = 2, ymin = -0.16, ymax = -0.04, alpha = .2, colour = "black")
-  
+	weight <- comLength/sum(comLength)
+      names(weight) <- c("5'UTR","CDS","3'UTR")
+
+  # density
+      cds_id <- which(ct1$comp=="CDS")
+      utr3_id <- which(ct1$comp=="UTR3")
+      utr5_id <- which(ct1$comp=="UTR5")
+      ct1$count[utr5_id] <- ct1$count[utr5_id]*weight["5'UTR"]
+      ct1$count[cds_id] <- ct1$count[cds_id]*weight["CDS"]
+      ct1$count[utr3_id] <- ct1$count[utr3_id]*weight["3'UTR"]
+      
+      # re-normalization
+      featureSet <- as.character(unique(ct$Feature))
+      for (i in 1:length(featureSet)) {
+        id <- (ct1$Feature==featureSet[i])
+        ct1$weight[id] <- ct1$count[id]/sum(ct1$count[id])
+      }
+      
+      # stratch
+      x <- cumsum(weight)
+      ct1$pos[utr5_id] <- ct1$pos[utr5_id]*weight["5'UTR"] + 0
+      ct1$pos[cds_id] <- ct1$pos[cds_id]*weight["CDS"] + x[1]
+      ct1$pos[utr3_id] <- ct1$pos[utr3_id]*weight["3'UTR"] + x[2]
+      
+      p1 <- 
+        ggplot(ct1, aes(x=pos, group=Feature, weight=weight))  +
+        ggtitle("Distribution on mRNA") +
+        theme(axis.ticks = element_blank(), axis.text.x = element_blank()) + 
+        xlab("") + 
+        ylab("Frequency") +
+        geom_density(adjust=adjust,aes(fill=factor(Feature),colour=factor(Feature)),alpha=0.2) +
+        annotate("text", x = x[1]/2, y = -0.2, label = "5'UTR") +
+        annotate("text", x = x[1] + weight[2]/2, y = -0.2, label = "CDS") +
+        annotate("text", x = x[2] + weight[3]/2, y = -0.2, label = "3'UTR") + 
+        theme(legend.position="bottom") +
+        geom_vline(xintercept= x[1:2], linetype="dotted") +
+        annotate("rect", xmin = 0, xmax = x[1], ymin = -0.12, ymax = -0.08, alpha = .99, colour = "black")+
+        annotate("rect", xmin = x[2], xmax = 1, ymin = -0.12, ymax = -0.08, alpha = .99, colour = "black")+
+        annotate("rect", xmin = x[1], xmax = x[2], ymin = -0.16, ymax = -0.04, alpha = .2, colour = "black")
   
   .multiplot(p1, p2, cols=2)
   
