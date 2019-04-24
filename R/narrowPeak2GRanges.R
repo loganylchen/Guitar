9e434603944a2e0334f84aba666b01576cfccdc3
jia.meng <jia.meng@hotmail.com>
2019-04-24 21:19:24 +0800

    Remove all files of Guitar v1

--- R/narrowPeak2GRanges.R
+++
@@ -1,9 +0,0 @@
-narrowPeaktoGRanges <- function(file) {
-  c <- read.table(file,sep="\t",header=FALSE)
-  peak <- GRanges(
-    seqnames=as.character(c[[1]]),
-    ranges=IRanges(start=c[[2]],end=c[[3]])
-  )
-  mcols(peak) <- c
-  return(peak)
-}
